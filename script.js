9670b03b965ec70695b4bb6b8a301434de7a396c
Gleepy <107324298+mtwence@users.noreply.github.com>
2022-06-24 15:20:29 -0500

    create arrays for password characters

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+// Input options as arrays
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var specChar = ["!", "%", "&", ",", "*", "+", "-", ".", "/", "<", ">", "?","~"];
+var caseUpper = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var caseLower = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+ 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+  while
+}
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +22,6 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+
+
